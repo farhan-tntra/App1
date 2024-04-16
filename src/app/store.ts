9eacdb57f6c6711d2cbfb8642965c7af7dede827
farhan <farhan.mithani@tntra.io>
2024-04-16 14:01:41 +0530

    updated logic and component

--- src/app/store.ts
+++ src/app/store.ts
@@ -12,23 +12,23 @@ export type RootState = ReturnType<typeof rootReducer>
 
 // The store setup is wrapped in `makeStore` to allow reuse
 // when setting up tests that need the same store config
-export const makeStore = (preloadedState?: Partial<RootState>) => {
-  const store = configureStore({
-    reducer: rootReducer,
-    // Adding the api middleware enables caching, invalidation, polling,
-    // and other useful features of `rtk-query`.
-    middleware: getDefaultMiddleware => {
-      return getDefaultMiddleware().concat(quotesApiSlice.middleware)
-    },
-    preloadedState,
-  })
-  // configure listeners using the provided defaults
-  // optional, but required for `refetchOnFocus`/`refetchOnReconnect` behaviors
-  setupListeners(store.dispatch)
-  return store
-}
+// export const makeStore = (preloadedState?: Partial<RootState>) => {
+//   const store = configureStore({
+//     reducer: rootReducer,
+//     // Adding the api middleware enables caching, invalidation, polling,
+//     // and other useful features of `rtk-query`.
+//     middleware: getDefaultMiddleware => {
+//       return getDefaultMiddleware().concat(quotesApiSlice.middleware)
+//     },
+//     preloadedState,
+//   })
+//   // configure listeners using the provided defaults
+//   // optional, but required for `refetchOnFocus`/`refetchOnReconnect` behaviors
+//   setupListeners(store.dispatch)
+//   return store
+// }
 
-export const store = makeStore()
+// export const store = makeStore()
 
 // Infer the type of `store`
 export type AppStore = typeof store
